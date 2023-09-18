0afd5f2db5b7e6c5929304ac194bef0330e6e9db
JackGitter <gitter.j@northeastern.edu>
2023-09-18 19:29:42 -0400

    wordle almost odone

--- background.js
+++ background.js
@@ -1,44 +1,40 @@
-/* chrome.runtime.onInstalled.addListener(async ({ reason }) => {
-    if (reason !== 'install') {
-      return;
-    }
-  
-    // Create an alarm so we have something to look at in the demo
-    await chrome.alarms.create('demo-default-alarm', {
-      delayInMinutes: .1,
-    });
-  });
+function timeToMillis(time) {
+  let today = new Date();
+  let timeArray = time.split(":")
+  let hours = parseInt(timeArray[0])
+  let minutes = parseInt(timeArray[1])
+  let currentHours = today.getHours();
+  let currentMinutes = today.getMinutes();
+  let currentSeconds = today.getSeconds();
+  hours -= currentHours;
+  minutes -= currentMinutes;
+  hours *= 3_600_000;
+  minutes *= 60000;
+  console.log(hours + minutes - (currentSeconds * 1000))
+  return hours + minutes - (currentSeconds * 1000);
+}
 
-*/
-
-// chrome.runtime.onStartup.addListener <-- if the alarm has expired, redirect
-
-  function timeToMillis(time) {
-    let today = new Date();
-    let timeArray = time.split(":")
-    let hours = parseInt(timeArray[0])
-    let minutes = parseInt(timeArray[1])
-    let currentHours = today.getHours();
-    let currentMinutes = today.getMinutes();
-    let currentSeconds = today.getSeconds();
-    hours -= currentHours;
-    minutes -= currentMinutes;
-    hours *= 3_600_000;
-    minutes *= 60000;
-    console.log(hours + minutes - (currentSeconds * 1000))
-    return hours + minutes - (currentSeconds * 1000);
-  }
-
-  chrome.runtime.onMessage.addListener(function(request, sender) {
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.game === 'connections') {
     chrome.storage.local.set({"time": request.time})
-    console.log(request.time)
-    chrome.alarms.create('connections alarm', {
+    chrome.alarms.create('connections', {
       when: Date.now() + timeToMillis(request.time),
       periodInMinutes: 1440
     })
+  } else {
+    chrome.storage.local.set({"timeWordle": request.time})
+    chrome.alarms.create('wordle', {
+     when: Date.now() + timeToMillis(request.time),
+     periodInMinutes: 1440
+    })
+  }
 });
 
-  chrome.alarms.onAlarm.addListener((alarm) => {
-    chrome.tabs.create({active: true, index: 0})
+chrome.alarms.onAlarm.addListener((alarm) => {
+  chrome.tabs.create({active: true, index: 0})
+  if (alarm.name === 'connections') {
     chrome.tabs.update({url: "https://www.nytimes.com/games/connections"});
-  });
+  } else {
+    chrome.tabs.update({url: "https://www.nytimes.com/games/wordle/index.html"})
+  }
+});
--- time.js
+++ time.js
@@ -1,11 +1,16 @@
 window.onload = async function() {
     let desiredTime = await chrome.storage.local.get('time')
+    let desiredTimeWorlde = await chrome.storage.local.get('timeWordle')
     document.getElementById('timeToPlay').value = desiredTime.time
+    document.getElementById('timeToPlayWordle').value = desiredTimeWorlde
 }
 
 document.getElementById('setBtn').onclick = async function() {
     let time = document.getElementById('timeToPlay').value
-    chrome.runtime.sendMessage({"time": time});
-    time = await chrome.storage.local.get('time');
+    chrome.runtime.sendMessage({"time": time, "game": "connections"});
 }
 
+document.getElementById('setBtnWordle').onclick = async function() {
+    let time = document.getElementById('timeToPlayWordle').value
+    chrome.runtime.sendMessage({"time": time, "game": "wordle"});
+}
